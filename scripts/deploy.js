fbce0cdda1c7f9c7e27bc9ab603dd65014478698
gedithejedi <gediminaspetreikis@yahoo.com>
2022-05-14 11:45:50 +0200

    simple nft contract template made that can be deployed on the localhost network

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,30 +1,25 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
-const hre = require("hardhat");
+const fs = require('fs');
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+	const GoodieBagNFT = await hre.ethers.getContractFactory('GoodieBagNFT');
+	const goodieBagNFT = await GoodieBagNFT.deploy('GoodieBagNFT', 'GBN');
 
-  // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+	await goodieBagNFT.deployed();
 
-  await greeter.deployed();
+	console.log('GoodieBagNFT deployed to:', goodieBagNFT.address);
 
-  console.log("Greeter deployed to:", greeter.address);
+	/* this code writes the contract addresses to a local */
+	/* file named config.js that we can use in the app */
+	fs.writeFileSync(
+		'./config.js',
+		`
+		export const contractAddress = "${goodieBagNFT.address}"
+  		export const ownerAddress = "${goodieBagNFT.signer.address}"
+  		`
+	);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+main().then(() => process.exit(0)).catch((error) => {
+	console.error(error);
+	process.exit(1);
 });
