2ed65f9e4a6d408d081c497153d668fb4a934abe
daeunYoon <nanuli00@naver.com>
2022-05-14 13:02:43 +0200

    Merge branch 'master' of https://github.com/JordyPouw/goodiebag

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,11 @@ module.exports = {
     browser: false,
     es2021: true,
     mocha: true,
-    node: true,
   },
   extends: [
-    "standard",
-    "plugin:prettier/recommended",
-    "plugin:node/recommended",
+    //"standard",
+    // "plugin:prettier/recommended",
+    // "plugin:node/recommended",
   ],
   parserOptions: {
     ecmaVersion: 12,
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <section className='Header'>
+
+        </section>
+    );
+};
+
+export default Header;
