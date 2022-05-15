7629618f823739b88eedf9d6d22a7933cd65d58e
daeunYoon <nanuli00@naver.com>
2022-05-15 16:07:11 +0200

    frontend header set up

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './Header.css';
+import styled from 'styled-components';
+import logoUrl from '../assets/logo.svg';
+import { ethers } from 'ethers';
+import Web3Modal from 'web3modal';
+import WalletConnectProvider from '@walletconnect/web3-provider';
+import { ownerAddress } from '../config';
+
+const ConnectButton = styled.a`
+    cursor: pointer;
+    border: solid black 1px;
+    border-radius: 2px;
+    padding: 10px 5px;
+    flex: 0;
+`
 
 const Header = () => {
     return (
         <section className='Header'>
-
+            <div className='container 5 max-w-screen-xl'>
+                <div className="logo"><img src={logoUrl} alt="goodibag_logo" /></div>
+                <ConnectButton>Connect</ConnectButton>
+            </div>
         </section>
     );
 };
