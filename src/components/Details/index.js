9af8ae4ef760e9b7e18e8f821ea73912cf467a7c
Jordy Pouw <jordypouw@gmail.com>
2022-05-21 01:26:50 +0200

    remove goodiebagfile; moved minting to detail page comopnent

--- src/components/Details/index.js
+++ src/components/Details/index.js
@@ -1,13 +1,24 @@
 import { useParams } from 'react-router-dom';
 
 import './details.css';
+import ActiveAccount from '../ActiveAccount';
+import { Mint } from '../Mint';
 
 export const BagDetails = () => {
   const { bagName, bagUuid } = useParams();
 
   return (
     <section className="s-bag-details">
-      {bagUuid ? <p>your bag: {bagUuid}</p> : <p>derp: {bagName}</p>}
+      {bagUuid ? (
+        <p>your bag: {bagUuid}</p>
+      ) : (
+        <>
+          <p>derp: {bagName}</p>
+          <ActiveAccount>
+            <Mint />
+          </ActiveAccount>
+        </>
+      )}
     </section>
   );
 };
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,17 +1,24 @@
 import { Link } from 'react-router-dom';
+import { useContractRead } from 'wagmi';
 
 import './main.css';
-import GoodieBag from '../GoodieBag';
-import ActiveAccount from '../ActiveAccount';
+import { useGoodieBag } from '../../hooks/useGoodieBag';
+import { useTransactionEffect } from '../../hooks/useTransactionCallback';
 
 export const Main = () => {
+  const { contractConfig } = useGoodieBag();
+  const totalSupply = useContractRead(contractConfig, 'totalSupply');
+  useTransactionEffect(totalSupply.refetch);
+
   return (
     <section className="s-main">
       <div className="banner"></div>
 
-      <div className="banner2"></div>
-
       <h2 className="heading">Discover our goodiebags!</h2>
+      <p className="subheading">
+        A total of {totalSupply.data?.toString()} goodiebags are currently
+        minted.
+      </p>
 
       <div className="bags">
         <Link className="bags__bag" to={`/discover/polygon-bag`}>
@@ -24,10 +31,6 @@ export const Main = () => {
           <p className="bags__bag-title">Polygon goodiebag</p>
         </Link>
       </div>
-
-      <ActiveAccount>
-        <GoodieBag />
-      </ActiveAccount>
     </section>
   );
 };
