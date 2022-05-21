dccb7938fb93a432843da5f583e8345987e7663e
daeunYoon <nanuli00@naver.com>
2022-05-22 00:43:02 +0200

    how to box design

--- src/components/HowTo/index.js
+++ src/components/HowTo/index.js
@@ -6,6 +6,14 @@ import './how_to.css';
 
 
 export const HowTo = ({ isOpen, handleClick, children }) => {
+
+    const howtoboxes = [
+        { title: 'Connect' },
+        { title: 'Discover' },
+        { title: 'Claim' },
+        { title: 'Learn' },
+    ]
+
     return (
         <>
             <div>
@@ -14,6 +22,15 @@ export const HowTo = ({ isOpen, handleClick, children }) => {
             <div className={classNames('how-to-pop-up', { active: isOpen })}>
                 <div className="s-close-pop-up" onClick={() => handleClick()}></div>
                 <h2 className='how-to-title'>How To</h2>
+                <div className='how-to-boxes'>
+                    {
+                        howtoboxes.map(box => (
+                            <div className='how-to-box'>
+                                <h3>{box.title}</h3>
+                            </div>
+                        ))
+                    }
+                </div>
             </div>
         </>
     );
