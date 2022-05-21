cebf2e86e400c3307009ab19984fe8daf9f1d3c0
gedithejedi <gediminaspetreikis@yahoo.com>
2022-05-21 23:43:52 +0200

    Added a background color to the pop up/ progresson my goodiebags page

--- src/components/Collection/index.js
+++ src/components/Collection/index.js
@@ -17,22 +17,35 @@ export const Collection = () => {
   );
   useTransactionEffect(userGoodieBags.refetch);
 
-  return (
-    <section className="s-collection">
-      <ActiveAccount inactiveState={<EmptyState />}>
-        <h2 className="heading">You own the following goodiebags!</h2>
 
-        <div className="bags">
-          {userGoodieBags.data?.map((token) => (
-            <Link className="bag" to={`/my-goodiebags/${token}`}>
-              <h2>Goodiebag {token}</h2>
-              <GoodieBagTokens tokenId={token} />
-            </Link>
-          ))}
-        </div>
-      </ActiveAccount>
-    </section>
-  );
+  if(userGoodieBags.data.length > 0){
+
+    return (
+      <section className="s-collection">
+        <ActiveAccount inactiveState={<EmptyState />}>
+          <div className="bags">
+            {userGoodieBags.data?.map((token) => (
+              <Link className="bag" to={`/my-goodiebags/${token}`}>
+                <h2>Goodiebag {token}</h2>
+                <GoodieBagTokens tokenId={token} />
+              </Link>
+            ))}
+          </div>
+        </ActiveAccount>
+      </section>
+    );
+
+  }else{
+
+    return (
+      <section className="s-collection">
+        <div className="banner"></div>
+        <h2 className="heading">You do not own any Goodiebags!</h2>
+        <h3 className="heading">Dont panic, You can get some</h3>
+      </section>
+    );
+
+  }
 };
 
 const EmptyState = () => (
