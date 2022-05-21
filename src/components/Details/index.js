905e3c287c65192e032fa00739c3794d814746a1
Jordy Pouw <jordypouw@gmail.com>
2022-05-21 02:03:26 +0200

    scafold detail view

--- src/components/Details/index.js
+++ src/components/Details/index.js
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 
 import './details.css';
 import ActiveAccount from '../ActiveAccount';
@@ -7,9 +7,39 @@ import { Redeem } from '../Redeem';
 
 export const BagDetails = () => {
   const { bagName, bagUuid } = useParams();
+  const { pathname } = useLocation();
+  const crumbs = pathname.split('/').filter(Boolean);
 
   return (
     <section className="s-bag-details">
+      <ul className="breadcrumbs">
+        {crumbs.map((c) => (
+          <li>{prettyName(c)}</li>
+        ))}
+      </ul>
+
+      <div className="split">
+        <div className="fancy-box">
+          {/* here comes the fancy goodiebag box with animation stuff? */}
+        </div>
+
+        <div className="info">
+          <h2 className="name">{prettyName(bagName)}</h2>
+
+          <article className="tokens">
+            <div className="token">
+              <p>derp 1</p>
+            </div>
+            <div className="token">
+              <p>derp 2</p>
+            </div>
+            <div className="token">
+              <p>derp 3</p>
+            </div>
+          </article>
+        </div>
+      </div>
+
       {bagUuid ? (
         <>
           <p>your bag: {bagUuid}</p>
@@ -17,7 +47,6 @@ export const BagDetails = () => {
         </>
       ) : (
         <>
-          <p>derp: {bagName}</p>
           <ActiveAccount>
             <Mint />
           </ActiveAccount>
@@ -26,3 +55,8 @@ export const BagDetails = () => {
     </section>
   );
 };
+
+function prettyName(name) {
+  const n = name[0].toUpperCase() + name.substring(1);
+  return n.replaceAll('-', ' ');
+}
