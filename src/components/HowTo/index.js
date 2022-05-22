7d06f0d6f87a97793b8b96a003df63e1d51c0c14
daeunYoon <nanuli00@naver.com>
2022-05-22 20:44:40 +0200

    how to boxes with svgs

--- src/components/HowTo/index.js
+++ src/components/HowTo/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import classNames from "classnames";
-
+import Connect from '../../assets/blockchain.svg';
+import Discover from '../../assets/astronaut.svg';
+import Claim from '../../assets/sale.svg';
+import Learn from '../../assets/brain.svg';
 
 import './how_to.css';
 
@@ -8,10 +11,10 @@ import './how_to.css';
 export const HowTo = ({ isOpen, handleClick, children }) => {
 
     const howtoboxes = [
-        { title: 'Connect', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ut tristique sapien. Phasellus vel augue suscipit, dapibus mi sed, mattis sapien. Pellentesque convallis velit ex, ullamcorper eleifend est ornare eu. Donec at ligula vitae dolor egestas fringilla in eget dui. Donec in est magna.' },
-        { title: 'Discover', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ut tristique sapien. Phasellus vel augue suscipit, dapibus mi sed, mattis sapien. Pellentesque convallis velit ex, ullamcorper eleifend est ornare eu. Donec at ligula vitae dolor egestas fringilla in eget dui. Donec in est magna.' },
-        { title: 'Claim', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ut tristique sapien. Phasellus vel augue suscipit, dapibus mi sed, mattis sapien. Pellentesque convallis velit ex, ullamcorper eleifend est ornare eu. Donec at ligula vitae dolor egestas fringilla in eget dui. Donec in est magna.' },
-        { title: 'Learn', desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ut tristique sapien. Phasellus vel augue suscipit, dapibus mi sed, mattis sapien. Pellentesque convallis velit ex, ullamcorper eleifend est ornare eu. Donec at ligula vitae dolor egestas fringilla in eget dui. Donec in est magna.' },
+        { title: 'Connect', img: Connect, desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ut tristique sapien. Phasellus vel augue suscipit, dapibus mi sed, mattis sapien. Pellentesque convallis velit ex, ullamcorper eleifend est ornare eu. Donec at ligula vitae dolor egestas fringilla in eget dui. Donec in est magna.' },
+        { title: 'Discover', img: Discover, desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ut tristique sapien. Phasellus vel augue suscipit, dapibus mi sed, mattis sapien. Pellentesque convallis velit ex, ullamcorper eleifend est ornare eu. Donec at ligula vitae dolor egestas fringilla in eget dui. Donec in est magna.' },
+        { title: 'Claim', img: Claim, desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ut tristique sapien. Phasellus vel augue suscipit, dapibus mi sed, mattis sapien. Pellentesque convallis velit ex, ullamcorper eleifend est ornare eu. Donec at ligula vitae dolor egestas fringilla in eget dui. Donec in est magna.' },
+        { title: 'Learn', img: Learn, desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ut tristique sapien. Phasellus vel augue suscipit, dapibus mi sed, mattis sapien. Pellentesque convallis velit ex, ullamcorper eleifend est ornare eu. Donec at ligula vitae dolor egestas fringilla in eget dui. Donec in est magna.' },
     ]
 
     return (
@@ -25,7 +28,7 @@ export const HowTo = ({ isOpen, handleClick, children }) => {
                 <div className='how-to-boxes'>
                     {
                         howtoboxes.map((box, i) => (
-                            <div className='how-to-box' key={i}>
+                            <div className='how-to-box' key={i} style={{ backgroundImage: `url(${box.img})` }}>
                                 <h3 style={{ paddingBottom: '5px' }}>{box.title}</h3>
                                 <p className='how-to-box-desc'>{box.desc}</p>
                             </div>
@@ -36,4 +39,3 @@ export const HowTo = ({ isOpen, handleClick, children }) => {
         </>
     );
 };
-
