997b85445885373ed5fc126f1d92219d952a799f
Roel Obdam <roel@stamkracht.com>
2022-05-22 12:30:41 +0200

    update tests

--- test/goodiebag-test.js
+++ test/goodiebag-test.js
@@ -1,8 +1,5 @@
 const { expect } = require('chai');
 const { ethers } = require('hardhat');
-const {
-  abi: WETH,
-} = require('@uniswap/v3-periphery/artifacts/contracts/interfaces/external/IWETH9.sol/IWETH9.json');
 
 const ETH = ethers.BigNumber.from((1e18).toString());
 
@@ -10,6 +7,18 @@ async function getWMatic() {
   return await ethers.getContractAt(
     'IERC20',
     '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270',
+  );
+}
+async function getWETH() {
+  return await ethers.getContractAt(
+    'IERC20',
+    '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619',
+  );
+}
+async function getAWMatic() {
+  return await ethers.getContractAt(
+    'IERC20',
+    '0x6d80113e533a2C0fe82EaBD35f1875DcEA89Ea97',
   );
 }
 
@@ -45,24 +54,34 @@ describe('GoodieBag', function () {
     goodieBag = await GoodieBag.deploy();
     await goodieBag.deployed();
   });
-  it('Mint goodiebag with 5 matic', async () => {
+  it('Mint goodiebag with 12 matic', async () => {
     const wmatic = await getWMatic();
-    await goodieBag.connect(owner).mint({ value: ETH.mul(10) });
+    await goodieBag.connect(owner).mint({ value: ETH.mul(12) });
     const wmaticOnToken = await goodieBag.getNFTTokenBalance(
       '0',
       wmatic.address,
     );
-    expect(wmaticOnToken).to.be.equal(ETH.mul(5));
+    expect(wmaticOnToken).to.be.equal(ETH.mul(4));
   });
   it('Redeem goodiebag', async () => {
     const wmatic = await getWMatic();
-    await goodieBag.connect(owner).mint({ value: ETH.mul(10) });
+    const awmatic = await getAWMatic();
+    const weth = await getWETH();
+    await goodieBag.connect(owner).mint({ value: ETH.mul(12) });
     await goodieBag.connect(owner).redeem('0');
-    expect(await wmatic.balanceOf(owner.address)).to.be.equal(ETH.mul(5));
+    expect(await wmatic.balanceOf(owner.address)).to.be.equal(
+      ETH.mul(4),
+    );
+    expect(await awmatic.balanceOf(owner.address)).to.be.gt(
+      ETH.mul(4),
+    );
+    expect(await weth.balanceOf(owner.address)).to.be.gt(
+      ETH.mul(0),
+    );
   });
   it('Should prevent non owners ro redeem', async () => {
     const wmatic = await getWMatic();
-    await goodieBag.connect(owner).mint({ value: ETH.mul(10) });
+    await goodieBag.connect(owner).mint({ value: ETH.mul(12) });
     expect(goodieBag.connect(account1).redeem('0')).to.be.revertedWith(
       'Not allowed',
     );
